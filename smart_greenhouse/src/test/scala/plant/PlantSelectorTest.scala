d1a196824001fd40858d7d434bdac2c99b54c40f
Anna <anna.vitali4@studio.unibo.it>
2022-07-23 18:26:36 +0200

    test [PlantselectorTest]: add scalaTest to verify PlantSelector functionalities

--- smart_greenhouse/src/test/scala/plant/PlantSelectorTest.scala
+++ smart_greenhouse/src/test/scala/plant/PlantSelectorTest.scala
@@ -1,2 +1,28 @@
 package plant
-class PlantSelectorTest {}
+
+import alice.tuprolog.Theory
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class PlantSelectorTest extends AnyFunSuite with Matchers:
+
+  private val PS = "Plant Selector"
+  private val plantSelector = PlantSelector.apply()
+
+  test(s"$PS should show all the possibile plants that can be cultivated") {
+    plantSelector.getAllAvailablePlants.size should be > 0
+  }
+
+  test(s"$PS should mantain the selected plants name") {
+    val plantIndex = 0
+    val selectedPlant = plantSelector.getAllAvailablePlants(plantIndex)
+    plantSelector.selectPlant(selectedPlant)
+    plantSelector.getPlantsSelectedName(plantIndex) should equal(selectedPlant)
+  }
+
+  test(s"$PS should mantain the selected plants identifier") {
+    val plantIndex = 0
+    val selectedPlant = plantSelector.getAllAvailablePlants(plantIndex)
+    plantSelector.selectPlant(selectedPlant)
+    plantSelector.getPlantsSelectedIdentifier.size should equal(plantSelector.getPlantsSelectedName.size)
+  }
