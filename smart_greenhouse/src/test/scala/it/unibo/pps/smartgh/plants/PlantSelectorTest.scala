963c8dfb069b7fc15ca2e148018dd6bf82fcf57b
Anna <anna.vitali4@studio.unibo.it>
2022-07-24 12:18:21 +0200

    test [PlantSelectorTest]: add new test for the new method deselectPlant

--- smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/plants/PlantSelectorTest.scala
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/plants/PlantSelectorTest.scala
@@ -4,6 +4,8 @@ import alice.tuprolog.Theory
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import java.util.NoSuchElementException
+
 /** This class contains the tests realized to verify that {@link PlantSelector} behaves correctly */
 class PlantSelectorTest extends AnyFunSuite with Matchers:
 
@@ -18,12 +20,31 @@ class PlantSelectorTest extends AnyFunSuite with Matchers:
     val plantIndex = 0
     val selectedPlant = plantSelector.getAllAvailablePlants(plantIndex)
     plantSelector.selectPlant(selectedPlant)
-    plantSelector.getPlantsSelectedName(plantIndex) should equal(selectedPlant)
+    plantSelector.getPlantsSelectedName(plantIndex) shouldEqual selectedPlant
+  }
+
+  test(s"$PS should allow the deselection of a plant") {
+    val plantIndex = 0
+    val selectedPlant = plantSelector.getAllAvailablePlants(plantIndex)
+    plantSelector.selectPlant(selectedPlant)
+    plantSelector.deselectPlant(selectedPlant)
+    plantSelector.getPlantsSelectedName.size shouldEqual 0
+  }
+
+  test(
+    "Calling the deselectPlant function with a plant that hasn't been selected should yield " +
+      "NoSuchElementException"
+  ) {
+    val plantIndex = 0
+    val notSelectedPlant = plantSelector.getAllAvailablePlants(plantIndex)
+    assertThrows[NoSuchElementException] {
+      plantSelector.deselectPlant(notSelectedPlant)
+    }
   }
 
   test(s"$PS should mantain the selected plants identifier") {
     val plantIndex = 0
     val selectedPlant = plantSelector.getAllAvailablePlants(plantIndex)
     plantSelector.selectPlant(selectedPlant)
-    plantSelector.getPlantsSelectedIdentifier.size should equal(plantSelector.getPlantsSelectedName.size)
+    plantSelector.getPlantsSelectedIdentifier.size shouldEqual plantSelector.getPlantsSelectedName.size
   }
