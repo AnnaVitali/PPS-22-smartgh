4f296bdfeea542985f5cee1ab84d8b71333e6700
Elena Yan <elena.yan@studio.unibo.it>
2022-07-27 12:42:25 +0200

    test [CitiesSearcher]: create CitiesSearcher test

---
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/city/CitiesSearcherTest.scala
@@ -0,0 +1,26 @@
+package it.unibo.pps.smartgh.city
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers as ShouldMatchers
+import org.scalatest.matchers.must.Matchers as MustMatchers
+
+class CitiesSearcherTest extends AnyFunSuite:
+
+  private val citiesSearcher = CitiesSearcher("/cities.pl")
+
+  test("Cities Searcher should show cities") {
+    import ShouldMatchers._
+    citiesSearcher.getAllCities.size should be > 0
+  }
+
+  test("Cities Searcher should correctly return a specific searched city") {
+    import ShouldMatchers._
+    val city = "Cesena"
+    citiesSearcher.searchCities(city) should contain(city)
+  }
+
+  test("Cities Searcher should correctly return searched cities starting with the specific chars") {
+    import MustMatchers._
+    val startWithChars = "F"
+    citiesSearcher.searchCities(startWithChars).foreach(c => c must startWith(startWithChars))
+  }
