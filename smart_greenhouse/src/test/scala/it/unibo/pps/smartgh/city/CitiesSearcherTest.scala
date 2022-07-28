f631a13fd52d6003f67026ae3daa02b26c4ed29f
Elena Yan <elena.yan@studio.unibo.it>
2022-07-28 17:20:37 +0200

    fix: fix CitiesSearcherTest and github workflow-jvm.yaml

--- smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/city/CitiesSearcherTest.scala
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/city/CitiesSearcherTest.scala
@@ -1,14 +1,23 @@
 package it.unibo.pps.smartgh.city
 
+import org.scalatest.BeforeAndAfter
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers as ShouldMatchers
 import org.scalatest.matchers.must.Matchers as MustMatchers
 
 /** This class contains the tests to verify that the [[CitiesSearcher]] work correctly. */
-class CitiesSearcherTest extends AnyFunSuite:
+class CitiesSearcherTest extends AnyFunSuite with BeforeAndAfter:
 
-  private val fileName = System.getProperty("user.home") + "/pps/cities.pl"
-  private val citiesSearcher = CitiesSearcher(fileName)
+  private val path = System.getProperty("user.home") + "/pps/"
+  private val file = "cities.txt"
+  private val prologFile = "cities.pl"
+  private val uploader = UploadCities
+  private var citiesSearcher: CitiesSearcher = _
+
+  before {
+    uploader.writePrologFile(path, file, prologFile)
+    citiesSearcher = CitiesSearcher(path + prologFile)
+  }
 
   test("Cities Searcher should show cities") {
     import ShouldMatchers._
