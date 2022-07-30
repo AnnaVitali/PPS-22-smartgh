98d6e00beae771fdec3308284ac9ba454c4ad198
Elena Yan <elena.yan@studio.unibo.it>
2022-07-30 17:08:44 +0200

    feat [timer]: implement timer

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/timer/Timer.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/timer/Timer.scala
@@ -1,54 +1,75 @@
 package it.unibo.pps.smartgh.timer
 
-import monix.execution.CancelableFuture
+import monix.execution.{Ack, Cancelable, CancelableFuture}
 
 import scala.concurrent.duration.*
 import monix.reactive.{Observable, OverflowStrategy}
 import monix.execution.Scheduler.Implicits.global
-import monix.execution.schedulers.TestScheduler.Task
-import org.joda.time.DateTime
+import monix.execution.schedulers.TestScheduler
+import monix.eval.Task
+import org.joda.time.{DateTime, Interval}
 
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration.Inf.toSeconds
 import scala.language.postfixOps
+import math.Integral.Implicits.infixIntegralOps
 
 trait Timer:
-  def subscribe(callback: FiniteDuration => Unit): CancelableFuture[Unit]
+  def value: Duration
+  def start(task: Duration => Unit): Unit
   def changeSpeed(speed: FiniteDuration): Unit
+  def stop(): Unit
 
 object Timer:
 
-  def apply(): Timer = TimerImpl()
+  def apply(duration: Duration): Timer = TimerImpl(duration)
 
-  class TimerImpl extends Timer:
-    var speed = 1 seconds
+  private class TimerImpl(duration: Duration) extends Timer:
+    var value: Duration = 0 seconds
+    var cancelable: Cancelable = _
+    var consumer: Duration => Unit = _
 
-    val timer: Observable[FiniteDuration] =
-      Observable
-        .fromIterable(
-          new Iterable[FiniteDuration]:
-            override def iterator: Iterator[FiniteDuration] =
-              new Iterator[FiniteDuration]:
-                var time: FiniteDuration = 0.hours + 0.minutes + 0.seconds
-                override def hasNext: Boolean = time != 23.hours + 59.minutes + 59.seconds
-                override def next(): FiniteDuration =
-                  time = time + 1.seconds
-                  time
-        )
-        .delayOnNext(speed)
-        .map(t => t)
-
-    override def subscribe(callback: FiniteDuration => Unit): CancelableFuture[Unit] =
-      timer.foreachL(callback).runToFuture
+    override def start(task: Duration => Unit): Unit =
+      consumer = t =>
+        value = t
+        task(t)
+      cancelable = timer(value, 1 seconds).runToFuture
 
     override def changeSpeed(s: FiniteDuration): Unit =
-      speed = s
+      stop()
+      cancelable = timer(value + 1.seconds, s).runToFuture
 
-@main def test =
-  val timer = Timer()
-  timer.subscribe(t => println(t))
+    override def stop(): Unit =
+      cancelable.cancel()
 
+    private def timer(from: Duration, speed: FiniteDuration): Task[Unit] =
+      Observable
+        .fromIterable(from.toSeconds to duration.toSeconds)
+        .throttle(speed, 1)
+        .map(Duration(_, TimeUnit.SECONDS))
+        .foreachL(consumer)
+
+@main def test(): Unit =
+  val timer = Timer(1.day)
+  timer.start(s =>
+    println(
+      "%d:%02d:%02d".formatted(s.toSeconds / 3600, (s.toSeconds % 3600) / 60, s.toSeconds % 60)
+    )
+  )
   Thread.sleep(5000)
 
-  println("change speed")
-  timer.changeSpeed(500 milliseconds)
+  println("change speed-------------")
+  timer.changeSpeed(100 milliseconds)
+  Thread.sleep(5000)
 
-  Thread.sleep(50000)
+  println("change speed-------------")
+  timer.changeSpeed(10 milliseconds)
+  Thread.sleep(5000)
+
+  println("change speed-------------")
+  timer.changeSpeed(1 nanoseconds)
+
+  Thread.sleep(5000)
+  timer.stop()
+  println("stop")
