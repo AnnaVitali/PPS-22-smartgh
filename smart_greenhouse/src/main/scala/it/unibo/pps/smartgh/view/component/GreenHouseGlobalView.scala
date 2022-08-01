5bd2cd3aa92b2e1560bad21a0c5f4fc3bc800fa4
Veronika Folin <veronika.folin@studio.unibo.it>
2022-08-01 18:53:42 +0200

    fix [GlobalGreenHouseView] fixed timerLabel's format

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/view/component/GreenHouseGlobalView.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/view/component/GreenHouseGlobalView.scala
@@ -4,18 +4,12 @@ import it.unibo.pps.smartgh.view.component.ViewComponent.AbstractViewComponent
 import javafx.scene.control.*
 import javafx.scene.layout.BorderPane
 
-import scala.concurrent.duration.*
-import scala.concurrent.duration.Duration.Inf.toSeconds
-import scala.language.postfixOps
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-
 trait GreenHouseGlobalView extends ViewComponent[BorderPane]:
 
   val greenHouseDivisionView: GreenHouseDivisionView
   type EnvironmentValues = Map[String, Any]
   def setEnvironmentValues(environmentValues: EnvironmentValues) : Unit
-  def setTimer(timerValue : FiniteDuration) : Unit
+  def setTimer(timerValue : String) : Unit
 
 object GreenHouseGlobalView:
 
@@ -28,20 +22,15 @@ object GreenHouseGlobalView:
     override val greenHouseDivisionView: GreenHouseDivisionView = GreenHouseDivisionView()
     component.setCenter(greenHouseDivisionView)
 
-    val timeFormat: DateFormat = new SimpleDateFormat("HH:mm:ss")
     val timeElapsedLabel : Label = component.lookup("#timeElapsedLabel").asInstanceOf[Label]
-    val defaultTimerValue : FiniteDuration = 0.hours + 0.minutes + 0.seconds
-    setTimer(defaultTimerValue)
 
     val speedSlider: Slider = component.lookup("#timeSpeedSlider").asInstanceOf[Slider]
     speedSlider.setOnMouseReleased(_ => notifySpeedChange(speedSlider.getValue))
 
     override def setEnvironmentValues(environmentValues: EnvironmentValues) : Unit = ???
 
-    override def setTimer(timerValue : FiniteDuration) : Unit =
-      System.out.println(timerValue.toSeconds)
-      val timerValueFormatted : String = timeFormat.format(timerValue.toSeconds)
-      timeElapsedLabel.setText(timerValueFormatted)
+    override def setTimer(timerValue : String) : Unit =
+      timeElapsedLabel.setText(timerValue)
 
     private def notifySpeedChange(value : Double) : Unit =
       // todo: richiamare metodo changeSpeed di TimeModel
