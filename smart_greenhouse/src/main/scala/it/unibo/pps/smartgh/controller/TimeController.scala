316ff55eb4004807a7ec7bd54b622f42dada78d8
Anna <anna.vitali4@studio.unibo.it>
2022-08-03 10:41:19 +0200

    feat [TimeController]: add implementation methods for the time controller

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/controller/TimeController.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/controller/TimeController.scala
@@ -1,16 +1,60 @@
 package it.unibo.pps.smartgh.controller
 
-trait TimeController:
-  def startSimulationTimer(): Unit
-  def stopSimulationTimer(): Unit
-  def updateVelocityTimer(speed: Double): Unit
+import it.unibo.pps.smartgh.model.time.TimeModel
+import it.unibo.pps.smartgh.view.component.GreenHouseGlobalView
 
+import scala.concurrent.duration.FiniteDuration
+
+/** A trait that represents the controller for the simulation timer. */
+trait TimeController:
+
+  /** Getter method for the view component.
+    * @return
+    *   the view assoociated to the controller.
+    */
+  def view: GreenHouseGlobalView
+
+  /** Setter method for the view component.
+    * @param view
+    *   the view associated to the controller.
+    */
+  def view_=(view: GreenHouseGlobalView): Unit
+
+  /** Method that notify the controller to start the simulation timer. */
+  def startSimulationTimer(): Unit
+
+  /** Method that notify the controller to stop the simulation timer. */
+  def stopSimulationTimer(): Unit
+
+  /** Method that notify the controller to update the speed of the simulation timer.
+    * @param speed
+    *   time that has to pass before emitting new items.
+    */
+  def updateVelocityTimer(speed: FiniteDuration): Unit
+
+  /** Method that requires the controller to update the time shown on the view.
+    * @param time
+    */
+  def update(time: String): Unit
+
+/** Object that can be used to create a new instance of [[TimeController]]. */
 object TimeController:
 
+  /** Create a new [[TimeController]].
+    * @return
+    *   a new instance of [[TimeController]].
+    */
   def apply(): TimeController = TimeControllerImpl()
 
   private class TimeControllerImpl extends TimeController:
 
-    override def startSimulationTimer(): Unit = ??? //TODO
-    override def stopSimulationTimer(): Unit = ??? //TODO
-    override def updateVelocityTimer(speed: Double): Unit = ??? //TODO
+    private val model = TimeModel()
+    override var view: GreenHouseGlobalView = _
+
+    override def startSimulationTimer(): Unit = model.start()
+
+    override def stopSimulationTimer(): Unit = model.stop()
+
+    override def updateVelocityTimer(speed: FiniteDuration): Unit = model.setSpeed(speed)
+
+    override def update(time: String): Unit = view.setTimer(time)
