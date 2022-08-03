1638e5a5e959e22ac65f9023ab1c7b5285a8f7e2
Maria <mengozzi.maria@gmail.com>
2022-08-03 17:49:39 +0200

    refactor: change test according to the new MVC Cake-Pattern

--- smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/model/greenhouse/GreenHouseTest.scala
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/model/greenhouse/GreenHouseTest.scala
@@ -1,14 +1,22 @@
 package it.unibo.pps.smartgh.model.greenhouse
 
+import it.unibo.pps.smartgh.controller.GHControllerModule
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
+import it.unibo.pps.smartgh.model.greenhouse.GHModelModule
+import it.unibo.pps.smartgh.view.component.GHViewModule
 
 /** This class contains the tests realized to verify the correct behavior of [[GreenHouseTest]]. */
-class GreenHouseTest extends AnyFunSuite with Matchers:
+class GreenHouseTest extends AnyFunSuite with Matchers
+  with GHModelModule.Interface
+  with GHViewModule.Interface
+  with GHControllerModule.Interface:
 
-  private val greenHouse =
-    GreenHouse(List("p1", "p2", "p3", "p4", "p5", "p6"), List("p1", "p2", "p3", "p4", "p5", "p6"), "Rome")
+  override val model: GHModelModule.Model = GreenHouseImpl(List("p1", "p2"), List("p1", "p2"), "Rome")
+  override val view: GHViewModule.View = GreenHouseDivisionViewImpl()
+  override val controller: GHControllerModule.Controller = GreenHouseDivisionControllerImpl()
 
-  test(s"greenhouse should have 2 rows and 3 columns") {
-    greenHouse.dimension mustEqual (2, 3)
+
+  test(s"greenhouse should have 1 rows and 2 columns") {
+    model.dimension mustEqual (1, 2)
   }
--- smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/view/component/GreenHouseDivisionViewTest.scala
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/view/component/GreenHouseDivisionViewTest.scala
@@ -1,6 +1,8 @@
 package it.unibo.pps.smartgh.view.component
 
-import it.unibo.pps.smartgh.model.greenhouse.GreenHouse
+import it.unibo.pps.smartgh.controller.GHControllerModule
+import it.unibo.pps.smartgh.model.greenhouse.GHModelModule
+import it.unibo.pps.smartgh.mvc.GreenHouseDivisionMVC.controller
 import it.unibo.pps.smartgh.view.component.*
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
@@ -20,20 +22,17 @@ import scalafx.scene.Scene
 @TestInstance(Lifecycle.PER_CLASS)
 @ExtendWith(Array(classOf[ApplicationExtension]))
 class GreenHouseDivisionViewTest extends AbstractViewTest:
-
+  import it.unibo.pps.smartgh.mvc.GreenHouseDivisionMVC
   val globalGH = "#ghDivision"
-  val model = GreenHouse(
-    List("p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11", "p12"),
-    List("p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11", "p12"),
-    "Rome"
-  )
+
 
   @Start
   private def start(stage: Stage): Unit =
     val baseView: BaseView = BaseView(appTitle, appSubtitle)
-    startApplication(stage, baseView, GreenHouseDivisionView())
+    startApplication(stage, baseView, GreenHouseDivisionMVC.view)
+
 
   @Test def testLabels(robot: FxRobot): Unit =
 
     verifyThat(globalGH, isVisible())
-    verifyThat(globalGH, hasChildren(model.plants.length, ""))
+    verifyThat(globalGH, hasChildren(GreenHouseDivisionMVC.model.plants.length, ""))
