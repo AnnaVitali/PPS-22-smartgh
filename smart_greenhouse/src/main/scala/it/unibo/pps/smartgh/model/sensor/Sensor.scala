b84443e5840134e596d446da9814eaeb320dd562
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-08-05 18:24:55 +0200

    feat [Sensor]: add new method to initialize the sensor

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/Sensor.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/Sensor.scala
@@ -1,6 +1,9 @@
 package it.unibo.pps.smartgh.model.sensor
 
+import monix.execution.Ack
+import scala.concurrent.Future
+
 object Sensor:
   trait Sensor:
-    def updateEnvironmentValue(): Unit
-    def registerValueCallback(f: Double => Unit): Unit
+    def initialize(area: Any): Unit //registrazione callback sull'area quando viene prodotto un nuovo valore
+    def registerValueCallback(onNext: Double => Future[Ack], onError: Throwable => Unit, onComplete: () => Unit): Unit
