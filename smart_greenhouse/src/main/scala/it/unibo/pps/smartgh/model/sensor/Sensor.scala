fa80547c890aac06064d514d86ab4be63178d1ca
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-08-06 12:46:14 +0200

    refactor [Sensor]: refactor interface sensor to add observable

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/Sensor.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/Sensor.scala
@@ -1,12 +1,16 @@
 package it.unibo.pps.smartgh.model.sensor
 
+import it.unibo.pps.smartgh.model.sensor.areaComponentsState.AreaComponentsState.AreaComponentsStateImpl
 import monix.execution.Ack
 import monix.reactive.Observable
+import it.unibo.pps.smartgh.model.sensor.areaComponentsState.AreaComponentsState
 
 import scala.concurrent.Future
 
 trait Sensor:
-  def initialize(
-      area: Observable[Any]
+  def setObserverEnvironmentValue(observableEnvironment: Observable[Double]): Unit
+
+  def setObserverActionsArea(
+      observableActionArea: Observable[AreaComponentsStateImpl]
   ): Unit //registrazione callback sull'area quando viene prodotto un nuovo valore o cambia lo stato
   def registerValueCallback(onNext: Double => Future[Ack], onError: Throwable => Unit, onComplete: () => Unit): Unit
