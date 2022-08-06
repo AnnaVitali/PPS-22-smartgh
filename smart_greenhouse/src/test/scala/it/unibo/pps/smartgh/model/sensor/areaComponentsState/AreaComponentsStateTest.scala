3133dd6387222e06e5817d653753fdcc896e5cc2
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-08-06 12:45:47 +0200

    tests [AreaComponentsStaTest]: add tests for the new object created

---
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/model/sensor/areaComponentsState/AreaComponentsStateTest.scala
@@ -0,0 +1,20 @@
+package it.unibo.pps.smartgh.model.sensor.areaComponentsState
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class AreaComponentsStateTest extends AnyFunSuite with Matchers:
+
+  private val areaComponentsState = AreaComponentsState()
+
+  test("At the beginning the area should be closed with shild up and nebulizer and ventilazion down") {
+    areaComponentsState.gatesState() shouldEqual AreaGatesState.Close
+    areaComponentsState.shildState() shouldEqual AreaShildState.Up
+    areaComponentsState.nebulizerState() shouldEqual AreaNebulizerState.NebulizationInactive
+    areaComponentsState.ventilationState() shouldEqual AreaVentilationState.VentilationInactive
+  }
+
+  test("At the beginning the lamp brightness should be initialized with the default value") {
+    val defaultBrightnessValue = 100
+    areaComponentsState.brightnessOfTheLamps() shouldEqual defaultBrightnessValue
+  }
