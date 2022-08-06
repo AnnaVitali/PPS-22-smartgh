9aa4517a0527d886314183e382b2f81e3626f1bb
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-08-06 12:47:34 +0200

    feat [LuminositySensor]: implement methods for luminosity sensor

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/LuminositySensor.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/LuminositySensor.scala
@@ -1,40 +1,96 @@
 package it.unibo.pps.smartgh.model.sensor
 
+import com.sun.javafx.webkit.theme.ContextMenuImpl
+import it.unibo.pps.smartgh.model.sensor.areaComponentsState.AreaComponentsState.AreaComponentsStateImpl
 import monix.execution.Ack
 import monix.reactive.Observable
+
 import scala.util.Random
+import monix.execution.Scheduler.Implicits.global
+import monix.reactive.subjects.ConcurrentSubject
+import monix.reactive.MulticastStrategy.Behavior
+import monix.reactive.MulticastStrategy
+import it.unibo.pps.smartgh.model.sensor.areaComponentsState.{AreaGatesState, AreaShildState}
+import it.unibo.pps.smartgh.model.sensor.areaComponentsState.AreaComponentsState.*
+import monix.execution.Ack.{Continue, Stop}
 
 import scala.concurrent.Future
 
 object LuminositySensor:
 
-  def apply(initialLuminosity: Int): LuminositySensorImpl = LuminositySensorImpl(initialLuminosity)
+  def apply(initialLuminosity: Double, areaComponentsState: AreaComponentsStateImpl): LuminositySensorImpl =
+    LuminositySensorImpl(initialLuminosity, areaComponentsState)
 
   object FactoryFunctionsLuminositySensor:
-    val computeLuminosityWithGreenhouseOpen: (currentValEnvironment: Int, currentLampBrightness: Int) => Int =
-      ???
-    val computeLuminosityWithGreenhouseCloseAndUnshild: (
-        currentValEnvironment: Int,
-        currentLampBrightness: Int
-    ) => Int = ???
-    val computeLuminosityWithGreenhouseCloseAndShild: (concurrentLampBrightness: Int) => Int = ???
-
-  class LuminositySensorImpl(initialLuminosity: Int) extends Sensor:
     private val minPercentage = 0.1
     private val maxPercentage = 0.5
-    private var currentValue: Int = (initialLuminosity - (minPercentage + (maxPercentage - minPercentage) * Random
-      .nextDouble()) * initialLuminosity).toInt
+    private val randomValue = Random(10)
 
-    override def initialize(
-        area: Observable[Any]
-    ): Unit = ??? //registrazione callback sull'area quando viene prodotto un nuovo valore o cambia lo stato
+    val computeLuminosityWithAreaGatesOpen: (currentValEnvironment: Double, currentLampBrightness: Int) => Double =
+      (valEnv, lampBrightness) => valEnv + lampBrightness
+
+    val computeLuminosityWithAreaGatesCloseAndUnshild: (
+        currentValEnvironment: Double,
+        currentLampBrightness: Int
+    ) => Double = (valEnv, lampBrightness) =>
+      valEnv - (minPercentage + (maxPercentage - minPercentage) * randomValue.nextDouble()) * valEnv + lampBrightness
+
+    val computeLuminosityWithAreaGatesCloseAndShild: (concurrentLampBrightness: Int) => Double = lampBrightness =>
+      lampBrightness.toDouble
+
+  class LuminositySensorImpl(initialLuminosity: Double, var areaComponentsState: AreaComponentsStateImpl)
+      extends Sensor:
+    private val randomValue = Random(10)
+    private val minPercentage = 0.1
+    private val maxPercentage = 0.5
+    private var currentEnvironmentValue: Double = _
+    private val subject: ConcurrentSubject[Double, Double] = ConcurrentSubject[Double](MulticastStrategy.publish)
+    private var currentValue: Double =
+      initialLuminosity - (minPercentage + (maxPercentage - minPercentage) * randomValue
+        .nextDouble()) * initialLuminosity
+
+    override def setObserverEnvironmentValue(observableEnvironment: Observable[Double]): Unit =
+      observableEnvironment.subscribe(onNextEnvironmentValue(), (ex: Throwable) => ex.printStackTrace(), () => {})
+
+    override def setObserverActionsArea(observableActionArea: Observable[AreaComponentsStateImpl]): Unit =
+      observableActionArea.subscribe(onNextAction(), (ex: Throwable) => ex.printStackTrace(), () => {})
 
     override def registerValueCallback(
         onNext: Double => Future[Ack],
         onError: Throwable => Unit,
         onComplete: () => Unit
-    ): Unit = ???
+    ): Unit = subject.subscribe(onNext, onError, onComplete)
 
-    def getCurrentValue: Int =
-      println(currentValue)
+    def getCurrentValue: Double =
       currentValue
+
+    private def computeNextSensorValue(): Unit =
+      areaComponentsState.gatesState() match
+        case AreaGatesState.Open =>
+          currentValue = FactoryFunctionsLuminositySensor.computeLuminosityWithAreaGatesOpen(
+            currentEnvironmentValue,
+            areaComponentsState.brightnessOfTheLamps()
+          )
+          println("area gates open and shild up")
+        case AreaGatesState.Close if areaComponentsState.shildState() == AreaShildState.Down =>
+          currentValue = FactoryFunctionsLuminositySensor.computeLuminosityWithAreaGatesCloseAndShild(
+            areaComponentsState.brightnessOfTheLamps()
+          )
+          println("area gates close and shild down")
+        case AreaGatesState.Close if areaComponentsState.shildState() == AreaShildState.Up =>
+          currentValue = FactoryFunctionsLuminositySensor.computeLuminosityWithAreaGatesCloseAndUnshild(
+            currentEnvironmentValue,
+            areaComponentsState.brightnessOfTheLamps()
+          )
+          println("area gates close and shild up")
+      subject.onNext(currentValue)
+
+    private def onNextAction(): AreaComponentsStateImpl => Future[Ack] = currentareaComponentsState =>
+      areaComponentsState = currentareaComponentsState
+      computeNextSensorValue()
+      Continue
+
+    private def onNextEnvironmentValue(): (newEnvironmentValue: Double) => Future[Ack] = envVal =>
+      currentEnvironmentValue = envVal
+      computeNextSensorValue()
+      Continue
