be3574d960b7cc419dedc73a851c27ab07608f37
Maria <mengozzi.maria@gmail.com>
2022-08-06 13:45:09 +0200

    doc: add documentation

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/area/ManageSensor.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/area/ManageSensor.scala
@@ -1,5 +1,5 @@
 package it.unibo.pps.smartgh.model.area
-//TODO DOC
+/** Implementation of the [[ManageSensor]]. */
 object ManageSensor:
   /**Class for manage a sensor. */
   case class ManageSensorImpl (name: String, min: Float, max: Float, sensor: Any, var actualVal: Float) //change sensorType
