e281b2e39286d7925a242dea2c6a8b055db2100f
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-08-06 09:23:48 +0200

    refactor [Sensor]: remove useless object for sensor

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/Sensor.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/model/sensor/Sensor.scala
@@ -1,9 +1,12 @@
 package it.unibo.pps.smartgh.model.sensor
 
 import monix.execution.Ack
+import monix.reactive.Observable
+
 import scala.concurrent.Future
 
-object Sensor:
-  trait Sensor:
-    def initialize(area: Any): Unit //registrazione callback sull'area quando viene prodotto un nuovo valore
-    def registerValueCallback(onNext: Double => Future[Ack], onError: Throwable => Unit, onComplete: () => Unit): Unit
+trait Sensor:
+  def initialize(
+      area: Observable[Any]
+  ): Unit //registrazione callback sull'area quando viene prodotto un nuovo valore o cambia lo stato
+  def registerValueCallback(onNext: Double => Future[Ack], onError: Throwable => Unit, onComplete: () => Unit): Unit
