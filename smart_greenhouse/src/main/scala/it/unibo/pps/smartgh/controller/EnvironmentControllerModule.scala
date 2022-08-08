bb7788b2ade9762ea9d5417dad7c63ffb53b0548
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-08-08 23:14:44 +0200

    fix [EnvironmentControllerModule]: fix cast problem, in order to make the timer simulation works correctly

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/controller/EnvironmentControllerModule.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/controller/EnvironmentControllerModule.scala
@@ -9,27 +9,29 @@ import it.unibo.pps.smartgh.mvc.GreenHouseDivisionMVC
 /** Object that encloses the controller module to manage ambient environment values and simulation time. */
 object EnvironmentControllerModule:
 
-  /** A trait that represents the controller for environment values and time management, it also allows their display in the view. */
+  /** A trait that represents the controller for environment values and time management, it also allows their display in
+    * the view.
+    */
   trait EnvironmentController:
 
     /** @return time controller. */
-    val timeController : TimeController
+    val timeController: TimeController
 
     /** Method that notify the controller to start the simulation. */
-    def startSimulation() : Unit
+    def startSimulation(): Unit
 
     /** Method that notify the controller to stop the simulation. */
-    def stopSimulation() : Unit
+    def stopSimulation(): Unit
 
     /** Method that notify the controller to request environment's model to update environment values.
-      *   @param hour
-      *     time value, expressed in hours, to which the update request refers.
-      *   */
-    def notifyEnvironmentValuesChange(hour: Int) : Unit
+      * @param hour
+      *   time value, expressed in hours, to which the update request refers.
+      */
+    def notifyEnvironmentValuesChange(hour: Int): Unit
 
   /** Trait that represents the provider of the controller for environment values and time management. */
   trait Provider:
-    val environmentController : EnvironmentController
+    val environmentController: EnvironmentController
 
   type Requirements = EnvironmentViewModule.Provider with EnvironmentModelModule.Provider
 
@@ -38,36 +40,41 @@ object EnvironmentControllerModule:
     context: Requirements =>
 
     /** Class that contains the [[EnvironmentController]] implementation. */
-    class EnvironmentControllerImpl(selectedPlants : List[Plant]) extends EnvironmentController:
+    class EnvironmentControllerImpl(selectedPlants: List[Plant]) extends EnvironmentController:
 
       val ghMVC = GreenHouseDivisionMVC(selectedPlants)
       environmentView.displayGreenHouseDivisionView(ghMVC.ghDivisionView)
 
-      override val timeController: TimeController = TimeController(context.environmentModel.time, context.environmentView, this)
+      override val timeController: TimeController =
+        TimeController(context.environmentModel.time, context.environmentView, this)
 
       override def startSimulation(): Unit =
         timeController.startSimulationTimer()
-        ghMVC.setAreas(environmentModel.time.timer,
-          Map("temp" -> environmentModel.subjectTemperature,
+        ghMVC.setAreas(
+          environmentModel.time.timer,
+          Map(
+            "temp" -> environmentModel.subjectTemperature,
             "lux" -> environmentModel.subjectLuminosity,
             "hum" -> environmentModel.subjectHumidity,
-            "soilMoist" -> environmentModel.subjectSoilMoisture))
+            "soilMoist" -> environmentModel.subjectSoilMoisture
+          )
+        )
         ghMVC.show()
-        
 
       override def stopSimulation(): Unit = timeController.stopSimulationTimer()
-      
+
       override def notifyEnvironmentValuesChange(hour: Int): Unit =
         context.environmentView.displayNameCity(environmentModel.city.name)
         context.environmentModel.city.updateCurrentEnvironmentValues(hour)
         context.environmentView.displayEnvironmentValues(environmentModel.city.currentEnvironmentValues)
         notifySensors(environmentModel.city.currentEnvironmentValues)
 
-      private def notifySensors(values: environmentModel.city.EnvironmentValues) : Unit =
+      private def notifySensors(values: environmentModel.city.EnvironmentValues): Unit =
         context.environmentModel.subjectTemperature.onNext(values("temp_c").asInstanceOf[Double])
-        context.environmentModel.subjectHumidity.onNext(values("humidity").asInstanceOf[Double])
-        context.environmentModel.subjectLuminosity.onNext(values("lux").asInstanceOf[Double])
-        context.environmentModel.subjectSoilMoisture.onNext(values("condition").asInstanceOf[Double])
+        context.environmentModel.subjectHumidity.onNext(values("humidity").asInstanceOf[BigInt].doubleValue)
+        context.environmentModel.subjectLuminosity.onNext(values("lux").asInstanceOf[Int].toDouble)
+        //context.environmentModel.subjectSoilMoisture.onNext(30.0) NON SERVE
+
 
   /** Trait that encloses the controller for environment values and time management. */
   trait Interface extends Provider with Component:
