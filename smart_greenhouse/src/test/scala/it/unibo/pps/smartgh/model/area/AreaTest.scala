89445ce808ea384a89111a145b161c42ca0b581c
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-08-09 10:50:10 +0200

    fix [AreaTest]: initialize timer in test

--- smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/model/area/AreaTest.scala
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/model/area/AreaTest.scala
@@ -1,25 +1,28 @@
 package it.unibo.pps.smartgh.model.area
 
 import it.unibo.pps.smartgh.model.plants.Plant
+import it.unibo.pps.smartgh.model.time.Timer
 import org.scalatest.funsuite.AnyFunSuite
+import scala.concurrent.duration.*
+
+import scala.language.postfixOps
 
 /** This class contains the tests to verify that the [[AreaModelModule]] works correctly. */
 class AreaTest extends AnyFunSuite with AreaModelModule.Interface:
 
-  override val areaModel = AreaImpl(Plant("lemon", "citrus limon"), null)
+  override val areaModel = AreaImpl(Plant("lemon", "citrus limon"), Timer(1 day))
 
-  test("After create an area in which there is a lemon plant, The area plant's name must be lemon"){
+  test("After create an area in which there is a lemon plant, The area plant's name must be lemon") {
     import org.scalatest.matchers.must.Matchers.mustEqual
     areaModel.plant.name mustEqual "lemon"
   }
 
-  test("After create an area its state must be equal to Normal"){
+  test("After create an area its state must be equal to Normal") {
     import org.scalatest.matchers.must.Matchers.mustEqual
     areaModel.status mustEqual AreaModelModule.AreaStatus.NORMAL
   }
 
-  test("An area must have 4 sensors"){
+  test("An area must have 4 sensors") {
     import org.scalatest.matchers.must.Matchers.mustEqual
     areaModel.sensors.size mustEqual 4
   }
-
