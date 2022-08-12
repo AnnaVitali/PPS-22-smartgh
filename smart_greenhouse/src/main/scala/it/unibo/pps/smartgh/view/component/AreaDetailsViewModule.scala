de4f2dac962e103bcac985ab780d2a1202909985
Elena Yan <elena.yan@studio.unibo.it>
2022-08-12 19:04:44 +0200

    feat [area]: start implement area details view

---
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/view/component/AreaDetailsViewModule.scala
@@ -0,0 +1,48 @@
+package it.unibo.pps.smartgh.view.component
+
+import it.unibo.pps.smartgh.controller.component.AreaControllerModule
+import it.unibo.pps.smartgh.view.SimulationViewModule.SimulationView
+import it.unibo.pps.smartgh.view.component.SelectPlantViewModule.SelectPlantView
+import it.unibo.pps.smartgh.view.component.ViewComponent.AbstractViewComponent
+import javafx.application.Platform
+import javafx.fxml.FXML
+import javafx.scene.Parent
+import javafx.scene.control.Label
+import javafx.scene.layout.VBox
+
+object AreaDetailsViewModule:
+
+  trait AreaDetailsView extends ViewComponent[VBox]:
+    def moveToNextScene[A <: Parent](viewComponent: ViewComponent[A]): Unit
+
+  trait Provider:
+    val areaDetailsViewModule: AreaDetailsView
+
+  type Requirements = AreaControllerModule.Provider
+
+  trait Component:
+    context: Requirements =>
+
+  class AreaDetailsViewImpl(private val simulationView: SimulationView, private val baseView: BaseView)
+      extends AbstractViewComponent[VBox]("area_details.fxml")
+      with AreaDetailsView:
+
+    override val component: VBox = loader.load[VBox]
+
+    @FXML
+    var plantNameLabel: Label = _
+
+    @FXML
+    var plantDescriptionLabel: Label = _
+
+    @FXML
+    var parametersVbox: VBox = _
+
+    Platform.runLater(() => baseView.changeSceneButton.setText("Back"))
+    baseView.changeSceneButton.setOnMouseClicked { _ => /*todo*/ }
+
+    override def moveToNextScene[A <: Parent](viewComponent: ViewComponent[A]): Unit =
+      simulationView.changeView(viewComponent)
+
+    trait Interface extends Provider with Component:
+      self: Requirements =>
