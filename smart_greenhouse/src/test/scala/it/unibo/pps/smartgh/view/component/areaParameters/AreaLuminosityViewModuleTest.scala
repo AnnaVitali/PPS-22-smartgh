f8e3437c9d4fce02d4042eb90087e76dc2750ab3
Elena Yan <elena.yan@studio.unibo.it>
2022-08-23 12:22:31 +0200

    test [luminosity]: add area luminosity parameter test

---
+++ smart_greenhouse/src/test/scala/it/unibo/pps/smartgh/view/component/areaParameters/AreaLuminosityViewModuleTest.scala
@@ -0,0 +1,38 @@
+package it.unibo.pps.smartgh.view.component.areaParameters
+
+import it.unibo.pps.smartgh.model.area.AreaModelModule.AreaModel
+import javafx.scene.control.Slider
+import javafx.stage.Stage
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.{Test, TestInstance}
+import org.junit.jupiter.api.TestInstance.Lifecycle
+import org.junit.jupiter.api.extension.ExtendWith
+import org.testfx.api.FxRobot
+import org.testfx.framework.junit5.{ApplicationExtension, Start}
+import org.testfx.matcher.control.ButtonMatchers
+
+/** This class contains the tests to verify that the [[AreaLuminosityViewModule]] work correctly. */
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(Array(classOf[ApplicationExtension]))
+class AreaLuminosityViewModuleTest extends AbstractAreaParametersViewTest("Luminosity", "Brightness"):
+
+  private val upShieldingBtnId = "#upShieldingBtn"
+  private val downShieldingBtnId = "#downShieldingBtn"
+  private val lampBrightnessSliderId = "#lampBrightnessSlider"
+  private val lampFactor = 1000.0
+  private var areaModel: AreaModel = _
+
+  @Start
+  override def start(stage: Stage): Unit =
+    super.start(stage)
+    areaModel = areaDetailsMVC.areaModel
+
+  @Test
+  def testButtons(robot: FxRobot): Unit =
+    basicToggleButtonTest(getToggleButton(robot, upShieldingBtnId), upShieldingBtnId, "Up", true)
+    basicToggleButtonTest(getToggleButton(robot, downShieldingBtnId), downShieldingBtnId, "Down", false)
+
+  @Test
+  def testSlider(robot: FxRobot): Unit =
+    val slider = robot.lookup(lampBrightnessSliderId).queryAs(classOf[Slider])
+    assertEquals(areaModel.getAreaComponent.brightnessOfTheLamps, slider.getValue * lampFactor)
