3d2beccf3a83997fa7d4bc8a8e495579bdc26f64
Elena Yan <elena.yan@studio.unibo.it>
2022-08-25 23:02:39 +0200

    doc [SceneController]: add scala doc

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/controller/component/SceneController.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/controller/component/SceneController.scala
@@ -1,5 +1,6 @@
 package it.unibo.pps.smartgh.controller.component
 
+/** A trait for the controllers to manage scenes. */
 trait SceneController:
 
   /** Method for the controller to doing something before next scene. */
