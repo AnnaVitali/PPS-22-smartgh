784f741a180ec651d3eceb5f2a0b41b9fb724793
Veronika Folin <veronika.folin@studio.unibo.it>
2022-09-02 12:20:35 +0200

    fix [Config]

--- smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/Config.scala
+++ smart_greenhouse/src/main/scala/it/unibo/pps/smartgh/Config.scala
@@ -1,9 +1,11 @@
 package it.unibo.pps.smartgh
 
+import java.io.File
+
 /** Configuration object. */
 object Config:
   /** Path to which prolog files are stored. */
-  val Path: String = System.getProperty("user.home") + "/pps/"
+  val Path: String = System.getProperty("user.home") + File.separator + "pps" + File.separator
   /** Cities input file name. */
   val CitiesInputFile: String = "cities.txt"
   /** Plants input file name. */
